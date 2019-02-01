e92f347fecf440d31ea226acebf726ec855fae8d
Apollo0406 <2433823963@qq.com>
2019-02-01 16:00:04 +0800

    finished controller OrderDetail and OrderCancel

---
+++ sell10-20190201/src/main/java/com/sell/dto/OrderDTO.java
@@ -0,0 +1,79 @@
+package com.sell.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.sell.entity.OrderDetail;
+import com.sell.utils.serializer.Date2LongSerializer;
+import lombok.Data;
+import java.math.BigDecimal;
+import java.util.Date;
+import java.util.List;
+
+//DTO在数据传输中专门用的对象
+//DTO --data transfer object
+@Data
+//@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class OrderDTO {
+
+   //订单id
+    private String orderId;
+
+    //买家名字
+    private String buyerName;
+
+    //买家手机号
+    private String buyerPhone;
+
+    //买家地址
+    private String buyerAddress;
+
+    //买家微信OpenId
+    private String buyerOpenid;
+
+    //订单总金额
+    private BigDecimal orderAmount;
+
+    //订单状态
+    private Integer orderStatus;
+
+    //支付状态
+    private Integer payStatus;
+
+    //创建时间
+    @JsonSerialize(using = Date2LongSerializer.class)
+    private Date createTime;
+
+    //更新时间
+    @JsonSerialize(using = Date2LongSerializer.class)
+    private Date updateTime;
+
+    //订单详情列表
+    List<OrderDetail> orderDetailList;
+
+
+    /* private String detailId;
+
+    //订单id
+    private String orderId;
+
+    //商品id
+    private String  productId;
+
+    //商品名称
+    private String productName;
+
+    //商品单价
+    private BigDecimal productPrice;
+
+    //商品小图
+    private String productIcon;
+
+    //商品数量
+    private Integer productQuantity;
+
+    //订单配送地址
+    private String buyerAddress;
+
+    private List<OrderDetail> orderDetailList;*/
+}
